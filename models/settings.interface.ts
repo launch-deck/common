0a0ede889319712cdbc83691de44cbf4ea8c7815
David Hamilton <nova706@gmail.com>
2023-04-06 16:09:23 -0600

    Add ClientSettings

--- models/settings.interface.ts
+++ models/settings.interface.ts
@@ -1,4 +1,12 @@
+export interface ClientSettings {
+    theme?: 'light' | 'dark'
+    backgroundImageUrl?: string
+    tileBlur?: number
+    tileAlpha?: number
+}
+
 export interface Settings {
     coreSettings: { [key: string]: string }
     pluginSettings: { [key: string]: { [key: string]: string } }
+    clientSettings: ClientSettings
 }
